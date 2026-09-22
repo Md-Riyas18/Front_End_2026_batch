ac4187d691b2cc8b0a58711009f1b3af6cc92acb
Riyas <rockey959738@gmail.com>
2026-09-22 21:43:30 +0530

    add files

---
+++ JAVASCRIPT/Day-2/pages/script.js
@@ -0,0 +1,237 @@
+//task 1
+
+var onBorading = true;
+console.log(onBorading);
+var standUp = 100;
+console.log(standUp);
+var sitDown = "right";
+console.log(sitDown);
+var netConnection = false;
+console.log(netConnection);
+var copyPaste = "ctrl+c";
+console.log(copyPaste);
+var backSpace = 300;
+console.log(backSpace);
+var afterEffects = 200;
+console.log(afterEffects);
+var upTrend = "trading";
+console.log(upTrend);
+var dailyTask = "coding";
+console.log(dailyTask);
+var marginTop = true;
+console.log(marginTop);
+var syncUp = 250;
+console.log(syncUp);
+var onSession = "zoom";
+console.log(onSession);
+var breakTime = 500;
+console.log(breakTime);
+var pushUp = "exercise";
+console.log(pushUp);
+var streamEngine = 10000;
+console.log(streamEngine);
+var sixPack = "abs";
+console.log(sixPack);
+var alightMotion = false;
+console.log(alightMotion);
+var fvgGaps = "gap";
+console.log(fvgGaps);
+var enterKey = "js";
+console.log(enterKey);
+var meetUp = 1000;
+console.log(meetUp);
+var karnaticMix = "music";
+console.log(karnaticMix);
+var genAi = true;
+console.log(genAi);
+var blackBox = 900;
+console.log(blackBox);
+var rdxGrapics = "3d";
+console.log(rdxGrapics);
+var snapDragon = 8000;
+console.log(snapDragon);
+var generalRules = "guidelines";
+console.log(generalRules);
+var workFlow = true;
+console.log(workFlow);
+var smcConcept = 300;
+console.log(smcConcept);
+var orderBlock = "block";
+console.log(orderBlock);
+var trendLine = false;
+console.log(trendLine);
+
+
+//task 2
+
+var x=10;
+var y=20;
+console.log(x + y);
+
+var x=10;
+var y=20;
+var z=x+y;
+console.log(z);
+
+var a=5;
+var b=15;
+var c=a*b;
+console.log(c);
+
+var p=100;
+var q=50;
+var r=p-q;
+console.log(r);
+
+var orderBlock ;
+console.log(orderBlock);
+
+
+
+//task 3
+
+//1 ) var keyword
+
+var studentName = "John";
+console.log(studentName); 
+
+var staffName = "riyas";
+console.log(staffName);
+
+var name = "rockey";
+console.log(name);
+
+var number=10;
+console.log(number);
+
+var x =5;
+var y =10;
+var z=x+y;
+console.log(z);
+
+var a=20;
+var b=60;
+console.log(a*b);
+
+var valueBoolean=true;
+console.log(valueBoolean);
+
+var valueBoolean=false;
+console.log(valueBoolean);
+
+var stringValue= null;
+console.log(stringValue);
+
+var numberValue=null;
+console.log(numberValue);
+
+var nullValue=null;
+console.log(nullValue);
+
+var string;
+console.log(string);
+
+var rollNo;
+console.log(rollNo);
+
+var age;
+console.log(age);
+
+
+//2 ) let keyword
+
+let youngsterName="roman";
+console.log(youngsterName);
+
+let youngsterAge="twenty";
+console.log(youngsterAge);
+
+let youngsterGender="male";
+console.log(youngsterGender);
+
+
+let youngsterHeight=6;
+console.log(youngsterHeight);
+
+let w=30;
+let s=40;
+let t=w+s;
+console.log(t);
+
+let k=1000;
+let l=500;
+let m=k*l;
+console.log(m);
+
+let f=200;
+let j=100;
+console.log(f-j);
+
+let collegeName=true;
+console.log(collegeName);
+
+let collegeLocation=false;
+console.log(collegeLocation);
+
+let collegeAddress=null;
+console.log(collegeAddress);
+
+let collegeCode=null;
+console.log(collegeCode);
+
+
+let collegeClass=null;
+console.log(collegeClass);
+
+let college;
+console.log(college);
+
+let collegeRollNo;
+console.log(collegeRollNo);
+
+
+
+
+//3 ) const keyword
+
+const internalBar = "bar1";
+console.log(internalBar);
+
+const externalBar = "bar2";
+console.log(externalBar);
+
+const liquidityConcept = "liquidity";
+console.log(liquidityConcept);
+
+const trendConcept = "trend";
+console.log(trendConcept);
+
+const niftyCompany=40;
+const bankNifty=20;
+console.log(niftyCompany + bankNifty);
+
+
+const forexCompany=400;
+const cryptoCompany=200;
+console.log(forexCompany*cryptoCompany);
+
+
+const SwingTrade=1000;
+const IntradayTrade=500;
+console.log(SwingTrade-IntradayTrade);
+
+
+const commodityGold="null";
+console.log(commodityGold);
+
+const commoditySilver="null";
+console.log(commoditySilver);
+
+const commodityBronze="null";
+console.log(commodityBronze);
+
+const commodityCopper=true;
+console.log(commodityCopper);
+
+const commodityCurdoil=false;
+console.log(commodityCurdoil);
