75012e4f589c6bc7e52324675f21c6fa81faece2
Riyas <rockey959738@gmail.com>
2026-09-23 16:36:27 +0530

    update

---
+++ JAVASCRIPT/Day-3/pages/script.js
@@ -0,0 +1,82 @@
+//string task :
+
+
+let stringName = "Riyas"
+stringName += " "
+console.log(stringName);
+stringName += "+"
+console.log(stringName);
+stringName += " "
+console.log(stringName);
+stringName += "Ibramsha"
+console.log(stringName);
+stringName += " "
+console.log(stringName);
+stringName += "+"
+console.log(stringName);
+stringName += " "
+console.log(stringName);
+stringName += "Mumtaj"
+console.log(stringName);
+stringName +=" "
+console.log(stringName);
+stringName += "are the family ."
+console.log(stringName);
+stringName += "They are lived together ."
+console.log(stringName);
+stringName += "The Mumtaj and Ibramsha are married ."
+console.log(stringName);
+stringName += "And then Riyas is the son of both Mumtaj and Ibramsha . "
+console.log(stringName);
+stringName +="After that another son is born he's name is Rahamatulla ."
+console.log(stringName);
+stringName += "Now there are 4 members ."
+console.log(stringName);
+stringName += "Riyas + Ibramsha + Mumtaj + Rahamatulla."
+console.log(stringName);
+stringName += "Now there are four menbers ."
+console.log(stringName);
+stringName += "Riyas is a elder one , he is recently completing college and doing course in chennai ."
+console.log(stringName);
+stringName += "Rahamathulla is younger one , he is currently studying B.Tech IT ."
+console.log(stringName);
+stringName -= "The Mumtaj and Ibramsha are married .And then Riyas is the son of both Mumtaj and Ibramsha . After that another son is born he's name is Rahamatulla .Now there are 4 members .Riyas + Ibramsha + Mumtaj + Rahamatulla.Now there are four menbers .Riyas is a elder one , he is recently completing college and doing course in chennai ."
+console.log(stringName);
+
+
+
+
+
+//number task
+
+let numberValue = 500
+numberValue += 100
+console.log(numberValue);
+numberValue += 400
+console.log(numberValue);
+numberValue -= 300
+console.log(numberValue);
+numberValue *= 2
+console.log(numberValue);
+numberValue /= 7
+console.log(numberValue);
+numberValue %= 2
+console.log(numberValue);
+numberValue **= 2
+console.log(numberValue);
+numberValue += 1000
+console.log(numberValue);
+numberValue += 2000
+console.log(numberValue);
+numberValue -= 500
+console.log(numberValue);
+numberValue *= 2
+console.log(numberValue);
+numberValue /= 50
+console.log(numberValue);
+numberValue += 900
+console.log(numberValue);
+numberValue -= 1000
+console.log(numberValue);
+
+
---
+++ JAVASCRIPT/training/pages/script.js
@@ -0,0 +1,41 @@
+let x = 5;
+let y= 10;
+console.log(x+y);
+
+
+let a = 15;
+let b = 20;
+let total = a+b;
+console.log(total);
+
+let g=10
+g+=20
+g+=50
+console.log(g);
+
+let f=50
+f-=10
+f+=30
+f*=2
+f/=7
+f%=14
+f**=2
+console.log(f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
