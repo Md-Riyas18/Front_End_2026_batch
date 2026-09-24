b601e97e8d80a4eeb33137b0b8bcad9801c5c2ce
Riyas <rockey959738@gmail.com>
2026-09-24 21:25:41 +0530

    update

---
+++ JAVASCRIPT/Day-4/pages/script.js
@@ -0,0 +1,46 @@
+// Task-1
+
+
+let numberValue = 10
+if( numberValue > 0){
+    console.log("Positive Number");
+}else{
+    console.log("Negative Number");
+    
+}
+
+
+// Task-2
+
+
+let voterAge = 20
+if (voterAge >= 18) {
+    console.log("Eligible to vote");
+}else{
+    console.log("Not eligible to vote");
+    
+}
+
+
+
+// Task-3
+
+
+let studentMark = 75
+if (studentMark >=90 ) {
+    console.log("Grade A");
+}else if (studentMark >=60) {
+    console.log("Grade B");
+}else if (studentMark < 60) {
+    console.log("Grade C");
+}else{
+    console.log("Fail");
+}
+
+
+
+//Task-4 
+
+for(let a = 1;a <= 10;a++){
+    console.log(a);
+}
--- JAVASCRIPT/training/pages/script.js
+++ JAVASCRIPT/training/pages/script.js
@@ -1,40 +1,65 @@
-let x = 5;
-let y= 10;
-console.log(x+y);
+// let x = 5;
+// let y= 10;
+// console.log(x+y);
 
 
-let a = 15;
-let b = 20;
-let total = a+b;
-console.log(total);
+// let a = 15;
+// let b = 20;
+// let total = a+b;
+// console.log(total);
 
-let g=10
-g+=20
-g+=50
-console.log(g);
+// let g=10
+// g+=20
+// g+=50
+// console.log(g);
 
-let f=50
-f-=10
-f+=30
-f*=2
-f/=7
-f%=14
-f**=2
-console.log(f)
-
-
-
+// let f=50
+// f-=10
+// f+=30
+// f*=2
+// f/=7
+// f%=14
+// f**=2
+// console.log(f)
 
 
 
 
 
 
+// let total = 100
+// let milkValue = 1
+// let chocolateValue = 2
+// let juiceValue = 3
+// if (milkValue >= 20 ) {
+//     console.log("milk is provided");
+    
+// }else if (chocolateValue >= 30) {
+//     console.log("chocolate is provided");
+    
+// }else if (juiceValue >= 50) {
+//     console.log("juice is provided");
+    
+// }else{
+//     console.log("insufficient");
+// }
+
+
+// let x = 22 
+// if (! x > 20) {
+//     console.log("true");
+    
+// }else{
+//     console.log("false");
+    
+// }
 
 
 
 
-
+let x = 10
+let y = "10"
+console.log(x === y);
 
 
 
